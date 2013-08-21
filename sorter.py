c432aa68fdad4e52a9147948ded986d5cbbc004f
QuantumApostle <yixingengcn@gmail.com>
2013-08-21 17:15:25 -0500

    finish main function without delete original files

--- sorter.py
+++ sorter.py
@@ -1,26 +1,51 @@
 import os
+import shutil
 import glob
 import string
 
-def printList(files):
-	for f in files:
-		# print f
-		# print str(f)
-		if "11" in str(f):
-			for c in str(f):
-				print c,
-			# pos = str(f).find('(')
-			# print str(f)[:pos]
+def fileList(files):
+        fileName = open("fileNames.txt", "w")
+        for f in files:
+                if ".mp3" in f:
+                        fileName.write(str(f) + "\n")
+        fileName.close()
+        print "file names exported"
+        
+def extractFileName(files):
 
-# def moveFile(files):
-	
-	
+        names = {}
+        for f in files:
+                if ".mp3" in f:
+                        a = f.decode("gbk")
+                        for i in range(len(a)):
+                                if a[i] == "ги".decode("gbk") or a[i].isdigit():
+                                        break
+                        if ".mp" not in a[:i]:
+                                names[a[:i]] = 0
+        return names.keys()
+
+def makeFolders(names):
+        for name in names:
+                newPath = homeDir + "\\" + name
+                if not os.path.exists(newPath):
+                        os.makedirs(newPath)
+                        
+def moveFile(files, names, homeDir):
+        for name in names:
+                for f in files:
+                        filePath = homeDir + "\\" + f.decode("gbk")
+                        dirPath = homeDir + "\\" + name
+                        
+                        fi = f.decode("gbk")
+                        if name in fi:
+                                shutil.copy(filePath, dirPath)
+        print "file movement finishes"
+
 
 	
 if __name__ == "__main__":
 	files = [f for f in os.listdir('.') if os.path.isfile(f)]
 	homeDir = os.getcwd()
-	print u'\u043e'
-	# printList(files)
-
-
+	names = extractFileName(files)
+	makeFolders(names)
+	moveFile(files, names, homeDir)
